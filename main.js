1e8042960c2f4d815f008a681f0f2e87d9de2996
Jun Goh <cygoh@umich.edu>
2019-07-29 16:41:32 -0700

    [feat] set up menu

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -11,4 +11,41 @@ app.on('ready', function() {
   // Create new window
   mainWindow = new BrowserWindow({});
   // Load html into window
-})
+  // file://dirname/mainWindow.html
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({label: ''});
+  }
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  mainWindow.webContents.openDevTools();
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+ // Create menu template
+ const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Item',
+      },
+      {
+        label: 'Remove Item'
+      },
+      {
+        label: 'Quit',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
